205df7466b2df0c2db32548c74fa717c5ec075b8
Suman <60940736+imSmaity@users.noreply.github.com>
2022-02-15 00:20:39 +0530

    -new

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import reactDom from "react-dom"
+
+
+
+function App(){
+    return <h1>Without create-react-app</h1>
+}
+
+reactDom.render(<App/>,document.getElementById('root'))
